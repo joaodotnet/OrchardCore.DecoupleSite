f3c7f6f08ed69ac30bc2e260d652e0d2a768eb04
João Gonçalves <x355505@bcpcorp.net>
2022-01-06 16:54:42 +0000

    Add project files.

---
+++ MyModule/AdminMenu.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Extensions.Localization;
+using OrchardCore.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyModule
+{
+    public class AdminMenu : INavigationProvider
+    {
+        private readonly IStringLocalizer S;
+
+        public AdminMenu(IStringLocalizer<AdminMenu> localizer)
+        {
+            S = localizer;
+        }
+
+        public Task BuildNavigationAsync(string name, NavigationBuilder builder)
+        {
+            // We want to add our menus to the "admin" menu only.
+            if (!String.Equals(name, "admin", StringComparison.OrdinalIgnoreCase))
+            {
+                return Task.CompletedTask;
+            }
+
+            // Adding our menu items to the builder.
+            // The builder represents the full admin menu tree.
+            builder
+                .Add(S["My Root View"], S["My Root View"].PrefixPosition(), rootView => rootView
+                   .Add(S["Child One"], S["Child One"].PrefixPosition(), childOne => childOne
+                       .Action("ChildOne", "DemoNav", new { area = "MyModule" }))
+                   .Add(S["Child Two"], S["Child Two"].PrefixPosition(), childTwo => childTwo
+                       .Action("ChildTwo", "DemoNav", new { area = "MyModule" })));
+
+            return Task.CompletedTask;
+        }
+    }
+}
